5413d5c8b26fd729dcf2904efa7bb2f094f666b5
tanim-k <tanimkhan877@gmail.com>
2022-02-26 12:24:43 +0600

    template multiline arrow operatins

--- es6-practice.js
+++ es6-practice.js
@@ -39,5 +39,42 @@ const word = 'i love you';
 const newFlowers =  `['belly', ${word}, 'rose','jui','joba', ${word}, ${word}, ]`;
 
 const newkolaLine = `${word}, is very strong. i love to use ${word}. ${word} is the way to shove love to one.${word} has the power of ${flowers.length*5}. ${word} is to hear good '${flowers[2]}'`;
+
 console.log('kola line:' ,newkolaLine);
 console.log(newFlowers);
+
+// 3: arrow function practice 
+// arrow - 1 :
+const gun = (num1, num2, num3) => num1*num2*num3;
+const multiplyResult = gun(2, 3, 4);
+console.log(multiplyResult);
+// arrow-2:
+const division = (num1, num2) => num1/ num2;
+const div = division(40,2);
+console.log(div);
+// arrow-3: only one parameter
+const name = last => 'fan ' + last;
+const muName = name('khan');
+console.log(muName);
+// arrow - 4 :
+const frndName = () => 'aditi' + ' ' + 'narayon'
+const aditi = frndName();
+console.log(aditi);
+// arrow-5 :
+const onePara = (num) => num/5;
+const oneParaRslt = onePara(100);
+console.log(oneParaRslt);
+// arrow - 6: multiline arrow
+const multilineArrow = (x, y) => {
+    const sum = x+y;
+    const multi = sum*sum;
+    return multi;
+}
+const multipleRslt = multilineArrow(2, 4);
+console.log(multipleRslt);
+// another arrow 
+const another = (p, q) => p+q;
+const result = another(10, 20)
+const squareRslt = (r) => r*r;
+const last = squareRslt(result); 
+console.log(last);
